c3f5bf29ab3630724ae1eb0d01c0cafdb248786e
Pedro Rabello <pedro.rabello@mercadolivre.com>
2019-02-25 13:25:47 -0300

    time to work

--- lib/islands_engine/island.ex
+++ lib/islands_engine/island.ex
@@ -9,4 +9,29 @@ defmodule IslandsEngine.Island do
   def new(), do:
     %Island{coordinates: MapSet.new(), hit_coordinates: MapSet.new()}
 
+  defp offset(:square), do: [{0,0},{0,1},{1,0},{1,1}]
+
+  defp offset(:atoll), do: [{0,0},{0,1},{1,1},{2,0},{2,1}]
+
+  defp offset(:dot), do: [{0,0}]
+
+  defp offset(:l_shape), do: [{0,0},{1,0},{2,0},{2,1}]
+
+  defp offset(:s_shape), do: [{0,1},{0,2},{1,0},{1,1}]
+
+  defp offset(_), do: {:error, :invalid_island_type}
+
+  defp add_coordinates(offsets, upper_left) do
+    Enum.reduce_while(offsets, MapSet.new(), fn offset,acc ->
+      add_coordinates(acc,upper_left,offset)
+    end)
+  end
+
+  defp add_coordinate(coordinates, %Coordinates{row:row, col: col}, {row_offset, col_offset}) do
+    case Coordinate.new(row+ row_offset, col+ col_offset) do
+        {:ok, coordinate} -> {:cont, MapSet.put(coordinates,coordinate)}
+        {:error, :invalid_coordinate} -> {:halt, {:error, :invalid_coordinate}}
+                                                          end
+  end
+
 end
