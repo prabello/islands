a846236f04ca6f47e0631595c14026625ff43122
Pedro Rabello <pedro.rabello@mercadolivre.com>
2019-02-25 07:58:49 -0300

    coordinate

--- lib/islands_engine/coordinate.ex
+++ lib/islands_engine/coordinate.ex
@@ -1,13 +1,22 @@
 defmodule IslandsEngine.Coordinate do
   # allow to make dirrect reference to a module without calling it eg: %Coordinate{} instead of %IslandsEgnine.Coordinate{}
-  alias __MODULE
+  alias __MODULE__
 #  @moduledoc false
 
   @enforce_keys [:row, :col] # makes the keys to be required when creating the structure
   defstruct [:row, :col]
 
-  def new(row,col) do
+  @board_range 1..10
+
+  def new(row,col) when row in(@board_range) and col in(@board_range) do
     {:ok, %Coordinate{row: row, col: col}}
   end
+
+  def new(_row,_col),do: {:error,:invalid_coordinate}
+
+#  Same as the one above
+#  def new(_row,_col) do
+#    {:error,:invalid_coordinate}
+#  end
 
 end
